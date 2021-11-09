0d04a83fe77b652fc29e7826d8acdcef7f28cf4a
PedroSebastiaan <pizarropedrov@gmail.com>
2021-11-09 17:04:24 -0300

    add: continue posts add

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#mybutton").click(function(){
+        $("#mybutton").slideUp("slow");
+        $("#form").show("slow");
+    });
+});
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @post = Post.new
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @posts = Post.all
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.js { render nothing: true, notice: 'Post was successfully created.' }
+      end
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
+
+
